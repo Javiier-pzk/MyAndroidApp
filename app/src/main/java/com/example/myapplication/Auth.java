3fdeedad28fd38fb793a4ad63c5e03e14383688d
Javier <javierphon@gmail.com>
2021-10-16 17:48:04 +0800

    Change Auth class methods to static, implement error handling and sendVerificationEmail upon user creation

--- app/src/main/java/com/example/myapplication/Auth.java
+++ app/src/main/java/com/example/myapplication/Auth.java
@@ -1,39 +1,88 @@
 package com.example.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
+import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
  * Represents a util class for firebase auth operations
  */
 public class Auth {
-    private final FirebaseAuth auth = FirebaseAuth.getInstance();
 
-    public void createNewUser(String email, String password, Context context) {
+    private static final String R_TAG = "Register Activity";
+    private static final String L_TAG = "Login Activity";
+    private static final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private static FirebaseUser user;
+    private static boolean isUserLoggedIn = false;
+
+    public static void createNewUser(String email, String password, Context context) {
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Utils.showToast(context, "Success! User created!");
+                        sendUserVerificationEmail();
+                        Utils.showShortToast(context, "Success! User created!");
                     } else {
-                        Utils.showToast(context, "Failed to create user!");
+                        onErrorCreateUser(task, context);
                     }
                 });
     }
 
-    public void loginUser(String email, String password, Context context) {
-        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
-            Utils.showToast(context, "Login Successful!");
-        });
+    public static void loginUser(String email, String password, Context context) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Utils.showShortToast(context, "Logged in successfully!");
+                    } else {
+                        onErrorLoginUser(task, context);
+                    }
+                });
     }
 
-    public void signOut(Context context) {
+    public static void signOut(Context context) {
         auth.signOut();
-        Utils.showToast(context, "Logged out successfully!");
+        Utils.showShortToast(context, "Logged out successfully!");
+    }
+
+    private static void sendUserVerificationEmail() {
+        user = auth.getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification();
+        }
+    }
+
+    private static void onErrorCreateUser(Task<AuthResult> task, Context context) {
+        try {
+            if (task.getException() != null) {
+                throw task.getException();
+            }
+        } catch (FirebaseAuthWeakPasswordException | FirebaseAuthUserCollisionException e) {
+            Utils.showLongToast(context, e.getMessage());
+        } catch (FirebaseAuthInvalidCredentialsException e) {
+            Utils.showLongToast(context, e.getMessage());
+        } catch (Exception e) {
+            Log.e(R_TAG, e.getMessage());
+        }
+    }
+
+    private static void onErrorLoginUser(Task<AuthResult> task, Context context) {
+        try {
+            if (task.getException() != null) {
+                throw task.getException();
+            }
+        } catch (FirebaseAuthInvalidUserException | FirebaseAuthInvalidCredentialsException e) {
+            Utils.showLongToast(context, e.getMessage());
+        } catch (Exception e) {
+            Log.e(L_TAG, e.getMessage());
+        }
     }
 }
 

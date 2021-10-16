e0ea1337e7427e11ff37592f3f76673fea628290
Javier <javierphon@gmail.com>
2021-10-16 17:48:33 +0800

    Add another util method for longer duration Toasts

--- app/src/main/java/com/example/myapplication/Utils.java
+++ app/src/main/java/com/example/myapplication/Utils.java
@@ -8,7 +8,11 @@ import android.widget.Toast;
  */
 public class Utils {
 
-    public static void showToast(Context context, String message) {
+    public static void showShortToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showLongToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 }
